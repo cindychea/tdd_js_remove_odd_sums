8ad4164b5fe508360849784c85390bb7a31a3bcd
cindychea <cindy_chea@hotmail.com>
2019-06-26 12:22:53 -0400

    completed testing reinforcement exercise

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
-function removeOddSum() {
+function removeOddSum(array, arg1, arg2) {
+    let evenSum = [];
+    array.forEach(element => {
+        if ((element[arg1] + element[arg2]) % 2 === 0) {
+            evenSum.push(element);
+        }
+    });
+    return evenSum
 }
 
 module.exports = removeOddSum;
